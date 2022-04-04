30e515b26eaf1c184684036564ca819d9bcc0ed8
hrithik.singh <hrithik.singh@bigohtech.com>
2022-04-04 17:51:00 +0530

    Deleted SW files from public

--- public/serviceWorker.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener("fetch", function () {
-  return;
-});
-
-// // All other custom service worker code goes here
-// // note: Here we are not using workbox config instead just 1 function her
-// // Just to trigger the install button
--- public/swRegistration.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("./serviceWorker.js")
-      .then((registration) => {
-        console.log("Service worker registered");
-      });
-  });
-}
